94ddcc130a9f9240dc0a65328bbb17580b15ebdf
nany <eguerrero@dlocal.com>
2022-04-18 14:08:21 -0300

    project saved config

--- src/test/com/restAssured/config/model/BaseTest.java
+++ src/test/com/restAssured/config/model/BaseTest.java
@@ -2,13 +2,16 @@ package restAssured.config.model;
 
 import io.restassured.RestAssured;
 import org.testng.annotations.BeforeTest;
+import utilsCommun.DataLoadPro;
 
 
 public class BaseTest {
 
+    DataLoadPro dataLoadPro;
+
     @BeforeTest
     public void setUp()
     {
-        RestAssured.baseURI="https://fakestoreapi.com";
+        RestAssured.baseURI=dataLoadPro.getDriverPath("uri");
     }
 }
